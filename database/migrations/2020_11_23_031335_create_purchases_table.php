e4b1e5c3c7574ba4e8b6b23f87f87459a90627ba
takumi-isd <caaarp339.fm@gmail.com>
2020-11-23 13:07:26 +0000

    add:purchases関連作成

---
+++ database/migrations/2020_11_23_031335_create_purchases_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('purchase_price')->unsigned();
+            $table->integer('purchase_quantity');
+            $table->string('purchase_company',128);
+            $table->timestamp('order_date')->useCurrent();
+            $table->timestamp('purchase_date')->useCurrent();
+            $table->integer('product_id')->unsigned();
+
+            // $table->foreign('product_id')->references('product_id')->on('m_products');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
