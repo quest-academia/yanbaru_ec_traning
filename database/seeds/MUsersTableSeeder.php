ec7fdf947ce69db34041bc19dc9698a7df7819a1
Troth IT <taroth827@gmail.com>
2020-11-24 19:20:28 +0900

    create_m_users及びseederファイル(仮) 作成

--- database/seeds/MUsersTableSeeder.php
+++ database/seeds/MUsersTableSeeder.php
@@ -11,9 +11,9 @@ class MUsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        DB::table('m_users')->insert([
             'password' => bcrypt('sample1'),
-            'last_name' => '佐藤',
+            'last_name' => '山本',
             'first_name' => '健',
             'zipcode' => '1111111',
             'prefecture' => '北海道',
@@ -22,6 +22,21 @@ class MUsersTableSeeder extends Seeder
             'apartments' => 'AAビル2F',
             'email' => 'sample1@sample.com',
             'phone_number' => '1111111111',
+            'user_classfication' => '1',
+            'company_name' => 'AA株式会社',
+            'delete_flag' => '0'
+        ]);
+        DB::table('m_users')->insert([
+            'password' => bcrypt('sample2'),
+            'last_name' => '熊本',
+            'first_name' => '健',
+            'zipcode' => '2222222',
+            'prefecture' => '熊本県',
+            'municipality' => '熊本市',
+            'address' => '1-2-3',
+            'apartments' => 'BBビル2F',
+            'email' => 'sample2@sample.com',
+            'phone_number' => '2222222222',
             'user_classfication' => '1',
             'company_name' => 'AA株式会社',
             'delete_flag' => '0'
