6aee4664bc77905d48a681ae2f0aff6bf7efd3d3
TAROTH666 <taroth827@gmail.com>
2020-11-25 09:46:34 +0000

    m_usersテーブル、及び5名分のテストデータ作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(M_ProductsSeeder::class);
+        $this->call(M_CategoriesSeeder::class);
+        $this->call(M_Products_StatusesSeeder::class);
+        $this->call(M_Sales_StatusesSeeder::class);
         $this->call(M_UsersTableSeeder::class);
     }
 }
