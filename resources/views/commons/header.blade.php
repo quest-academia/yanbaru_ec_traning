338cebe64248ae3f48a439443d30b26d63d81cf4
Torippana <17624328+Torippana@users.noreply.github.com>
2020-11-25 23:56:56 +0900

    [fix]インデント調整

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,13 +1,15 @@
 <header>
-    <h1><a href="#">やんばるエキスパート</a></h1>
+    <h1>
+        <a href="#">やんばるエキスパート</a>
+    </h1>
     <p>〇〇 ××さん</p>
-  <nav>
-    <ul>
-      <li><a href="#">商品検索</a></li>
-      <li><a href="#">カート</a></li>
-      <li><a href="#">注文履歴</a></li>
-      <li><a href="#">ユーザ情報</a></li>
-      <li><a href="#">ログアウト</a></li>
-    </ul>
-  </nav>
-  </header>
+    <nav>
+        <ul>
+            <li><a href="#">商品検索</a></li>
+            <li><a href="#">カート</a></li>
+            <li><a href="#">注文履歴</a></li>
+            <li><a href="#">ユーザ情報</a></li>
+            <li><a href="#">ログアウト</a></li>
+        </ul>
+    </nav>
+</header>
