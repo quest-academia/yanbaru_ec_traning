03a4dbcd2f6bb986c480a7d001fb0980ff907efe
TAROTH666 <taroth827@gmail.com>
2020-11-26 06:20:20 +0000

    外部キー制約をコメントアウト

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
         $this->call(M_ProductsSeeder::class);
         $this->call(M_CategoriesSeeder::class);
         $this->call(M_Products_StatusesSeeder::class);
