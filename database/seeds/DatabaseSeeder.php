0dc06b8854e0d898ac70509ded8614bab5bed6bc
yoshitaro-yoyo <tsute3842@gmail.com>
2020-11-27 14:58:45 +0000

    add:共通ビュー作成 blade&ルーティングの実装 再Push

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
-        $this->call([
-            M_CategoriesTableSeeder::class, 
-            M_Sales_StatusesTableSeeder::class, 
-            M_Products_StatusesTableSeeder::class, 
-            M_ProductsTableSeeder::class, 
-        ]);
+        // $this->call(UsersTableSeeder::class);
     }
 }
