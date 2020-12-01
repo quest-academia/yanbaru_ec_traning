c9523339c677588a833bb73b51312f4600c6784e
Torippana <17624328+Torippana@users.noreply.github.com>
2020-12-01 22:46:50 +0900

    ルーティングの設定とredirect先の設定完了

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <main>
+        <div class="container mt-5 text-center">
+            <h2>ログイン画面</h2>
+        </div>
+        {!! Form::open(['route' => 'login.post']) !!}
+            <form>
+                <div class="form-group">
+                    <div class="row justify-content-center">
+                        <div class="cal-xs-4">
+                            {!! Form::label('email', 'メールアドレス', ['class' => 'mt-3']) !!}
+                            {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="row justify-content-center">
+                    <div class="cal-xs-4">
+                        {!! Form::label('password', 'パスワード', ['class' => 'mt-3']) !!}
+                        {!! Form::password('password', ['class' => 'form-control']) !!}
+                    </div>
+                    </div>
+                </div>
+
+                <div class="text-center">
+                    {!! Form::submit('ログイン', ['class' => 'btn btn-primary']) !!}
+                </div>
+
+                <div class="text-center mt-3">
+                    <button type="button" class="btn btn-link">まだ登録がお済みでない方はこちら</button>
+                </div>
+            </form>
+        {!! Form::close() !!}
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+| ログイン機能
+|--------------------------------------------------------------------------
+*/
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+/*
+|--------------------------------------------------------------------------
+| ルートディレクトリへのアクセス時
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
-    return view('login/login');
+    return redirect('/home');
+});
+Route::get('/hoge', function () {
+    return view('home');
+});
+
+/*
+|--------------------------------------------------------------------------
+| ログイン後
+|--------------------------------------------------------------------------
+*/
+Route::group(['middleware' => 'auth:web'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
 });
