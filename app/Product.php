c3248c1443b5fcb7c7983a40f92bbccacd914f87
keisuke-1-7 <keisuke17.business@gmail.com>
2020-12-17 15:08:16 +0000

    カテゴリ以外の商品情報表示-完了

---
+++ app/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+
+class Product extends Model
+{
+    protected $table = 'm_products';
+    
+    //いる？
+    protected $fillable = [
+        'id',
+        'product_name',
+        'category_id',
+        'price',
+        'desecription',
+        'sale_status_id',
+        'regist_date',
+        'user_id',
+        'delete_flag',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //この記載の意味は？fillableとどちらかで良い？
+    //protected $guarded = array('id');
+
+    //public $timestamps = false;
+
+    public function getData(){
+        //ここの$dataはどこで使う？条件絞りたい時に使う？
+        $data = DB::table($this->table)->get();
+        return $data;
+    }
+
+}
---
+++ resources/views/iteminfo.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+    <main>
+        <div class="container">
+
+            <div class="mx-auto">
+                <h2>商品情報</h2>
+                <h3>
+                    @if(isset($pd_info->product_name))
+                            {{ $pd_info->product_name }}
+                    @endif
+                </h3>
+                <div class="row">
+                    <div class="col-sm">
+                        <div>
+                            <p class="itemCategory">商品カテゴリ:食料品</p>
+                            <p>商品説明:</p>
+                            <p>
+                                @if(isset($pd_info->description))
+                                    {{ $pd_info->description }}
+                                @endif
+                            </p>
+                            <br>
+                            <p>価格:
+                                @if(isset($pd_info->price))
+                                    {{ $pd_info->price }}
+                                @endif
+                                円
+                            </p>
+                        </div>
+                        {!! Form::open(['route' => 'signup.post']) !!}
+                        <form class="quantitySelection" action="" method="post">
+                            <div class="form-group row justify-content-center">
+                                <label class="col-form-label">購入個数</label>
+                                <div class="">
+                                    <!--ここをlaravelcollectiveに変更すること-->
+                                    <input type="number" class="inputNumber form-control">
+                                </div>
+
+                                <lavel class="col-form-label">個</lavel>
+                                <div class="col-sm-auto">
+                                    {!! Form::submit('カートへ', ['class' => 'btn btn-primary']) !!}
+                                    
+                                </div>
+                            </div>
+                        {!! Form::close() !!}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    </main>
+
+@endsection
