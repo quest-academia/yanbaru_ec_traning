f573415bebb34ac8703964630ec928e41d140519
Troth IT <taroth827@gmail.com>
2020-12-29 14:22:21 +0900

    コメントアウト追加

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,19 +10,19 @@ use Illuminate\Support\Facades\Auth;
 class CartController extends Controller
 {
     /*==================================
-    商品をカートに入れるメソッド(searchproduct)
+    商品をカートに入れるメソッド(iteminfo/id)
     ==================================*/
     //まず、商品詳細画面でカートに商品と個数を入れる処理をする
     public function addCart(Request $request)
     {
         //セッションに保存したい変数を定義する（ここでは商品idと注文個数）
         //飛んできた$requestの中のname属性をそれぞれ指定
-        $sessionProductId = $request->productId;
-        $sessionProductQuantity = $request->quantity;
+        $sessionProductId = $request->productId; //商品id
+        $sessionProductQuantity = $request->quantity; //注文個数
         //配列の入れ物を作る（初期化）
         $sessionData = array();
 
-        //作った配列に、compact関数を用いてidと個数の変数をまとめる（”” を使っているが変数の意味）
+        //作った配列に、compact関数を用いてidと個数の変数をまとめる（””は変数の意味）
         $sessionData = compact("sessionProductId", "sessionProductQuantity");
 
         //session_dataというキーで、$sessionDataをセッションに登録
