6f688e61c80e05a5321bb20a716ab8e9d7d6a942
gyagariko31 <gyagariko31@yahoo.co.jp>
2021-01-10 05:22:26 +0000

    t_purchase_tableのマイグレーション作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    o
                 </div>
 
                 <div class="links">
