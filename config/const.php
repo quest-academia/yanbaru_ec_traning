99cac741ae495acc3aaf6f9f3711dbba9d58a65e
Torippana <17624328+Torippana@users.noreply.github.com>
2021-01-11 23:04:17 +0900

    [remove]不要定数削除

--- config/const.php
+++ config/const.php
@@ -1,8 +1,5 @@
 <?php
 return [
-    // ログイン者のロールによる共通部分のテーマ切り替え用
-    'SELLER_MODE' => 'dark',
-    'BUYER_MODE'  => 'light',
     // ユーザーのロール定義
     'USER_CLASSIFICATIONS' => [
         'SELLER' => 1, // 出品者
