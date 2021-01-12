7ad5efb90581736c17e38681b49aa6406c89e1f6
miyazaki0505 <mr.gingerman0505@gmail.com>
2021-01-12 18:41:57 +0000

    add:ユーザ関連テーブル

---
+++ database/migrations/2021_01_12_181029_create_m_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('password', 64);
+            $table->string('last_name', 16);
+            $table->string('first_name', 16);
+            $table->integer('zipcode');
+            $table->string('prefecture', 16);
+            $table->string('municipality', 16);
+            $table->string('address', 32);
+            $table->string('apartments', 32);
+            $table->string('email', 128);
+            $table->integer('phone_number');
+            $table->unsignedInteger('user_classification_id');
+            $table->string('company_name', 128);
+            $table->char('delete_flag', 1);
+
+            $table->foreign('user_classification_id')->references('id')->on('m_user_classifications')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_users');
+    }
+}
