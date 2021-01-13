1825d0529170aed23579471a67c5631d1aa98118
hirotera <pomme-de-terre@hotmail.co.jp>
2021-01-13 23:30:45 +0900

    テストデータ内容修正

--- database/seeds/m_product_statusesTableSeeder.php
+++ database/seeds/m_product_statusesTableSeeder.php
@@ -12,7 +12,18 @@ class m_product_statusesTableSeeder extends Seeder
     public function run()
     {
         DB::table('m_product_statuses')->insert([
-            'product_status_name' => 'test',
+            [
+                'product_status_name' => '在庫あり',
+            ],
+            [
+                'product_status_name' => '品切れ中',
+            ],
+            [
+                'product_status_name' => '入荷待ち',
+            ],
+            [
+                'product_status_name' => '入荷未定',
+            ],
         ]);
     }
 }
--- database/seeds/m_sale_statusesTableSeeder.php
+++ database/seeds/m_sale_statusesTableSeeder.php
@@ -12,7 +12,15 @@ class m_sale_statusesTableSeeder extends Seeder
     public function run()
     {
         DB::table('m_sale_statuses')->insert([
-            'sale_status_name' => 'test',
+            [
+                'sale_status_name' => '販売中',
+            ],
+            [
+                'sale_status_name' => '販売中止',
+            ],
+            [
+                'sale_status_name' => '予約受付中',
+            ],
         ]);
     }
 }
