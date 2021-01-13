151ed31f640aea3ae7e6282e0d23b5dea49f31be
gyagariko31 <gyagariko31@yahoo.co.jp>
2021-01-13 21:18:17 +0000

    マイグレーションブランチ修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    o
+                    Laravel
                 </div>
 
                 <div class="links">
