88fca582879dea24534095cf090a6e7167f003c5
tomo-rrow86 <yu.tomo.ap1@gmail.com>
2021-01-14 10:11:30 +0000

    update:seederファイル インデント修正

--- database/seeds/T_OrdersTableSeeder.php
+++ database/seeds/T_OrdersTableSeeder.php
@@ -13,16 +13,16 @@ class T_OrdersTableSeeder extends Seeder
     {
         DB::table('t_orders')->insert([
             [
-            'user_id' => 1,
-            'order_date' => date('Y-m-d H:i:s'),
+                'user_id' => 1,
+                'order_date' => date('Y-m-d H:i:s'),
             ],
             [
-            'user_id' => 2,
-            'order_date' => date('Y-m-d H:i:s'),
+                'user_id' => 2,
+                'order_date' => date('Y-m-d H:i:s'),
             ],
             [
-            'user_id' => 3,
-            'order_date' => date('Y-m-d H:i:s'),
+                'user_id' => 3,
+                'order_date' => date('Y-m-d H:i:s'),
             ],
         ]);
     }
