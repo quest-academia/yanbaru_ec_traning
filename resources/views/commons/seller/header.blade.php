5d71ac47dc99db0b001098fde9902eea9add2f76
keisuke-1-7 <keisuke17.business@gmail.com>
2021-01-27 12:47:09 +0000

    管理者ヘッダーリンクの修正（仮）

--- resources/views/commons/seller/header.blade.php
+++ resources/views/commons/seller/header.blade.php
@@ -6,8 +6,8 @@
         <p>{{ !empty(Auth::user()) ? Auth::user()->last_name . Auth::user()->first_name : 'ユーザー' }} さん</p>
         <nav>
             <ul>
-                <li class="nav-item"><a href="" class="nav-link">出品者ページA</a></li>
-                <li class="nav-item"><a href="" class="nav-link">出品者ページB</a></li>
+                <li class="nav-item">{!! link_to_route('o_history', '商品出品', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('o_history', '商品検索', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item">{!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
         </nav>
     @else
