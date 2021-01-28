473f8aeaf88a8852abcb610109d36b35e0ac7bf9
keisuke-1-7 <keisuke17.business@gmail.com>
2021-01-28 13:51:04 +0000

    バックヘッダーリンクの修正

--- resources/views/commons/seller/header.blade.php
+++ resources/views/commons/seller/header.blade.php
@@ -7,7 +7,7 @@
         <nav>
             <ul>
                 <li class="nav-item">{!! link_to_route('o_history', '商品出品', [], ['class' => 'nav-link']) !!}</li>
-                <li class="nav-item">{!! link_to_route('o_history', '商品検索', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('seller_search', '商品検索', [], ['class' => 'nav-link']) !!}</li>
                 <li class="nav-item">{!! link_to_route('logout', 'ログアウト', [], ['class' => 'nav-link']) !!}</li>
         </nav>
     @else
