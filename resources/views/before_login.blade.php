1feaa079dd7216110b2488b1ab758d4fea84b46d
keisuke-1-7 <keisuke17.business@gmail.com>
2021-01-31 10:25:26 +0000

    before_login文字サイズ変更

--- resources/views/before_login.blade.php
+++ resources/views/before_login.blade.php
@@ -8,11 +8,11 @@
 
         <div class="row justify-content-center mt-5">
             <div class="col-3 text-center">
-                <h3>まだアカウントを<br>お持ちでない方はこちら</h3>
+                <h4>まだアカウントを<br>お持ちでない方はこちら</h4>
                 {!! link_to_route('signup', '新規登録', [], ['class' => 'btn btn-primary btn-lg']) !!}
             </div>
             <div class="col-3 text-center">
-                <h3>すでにアカウントを<br>お持ちの方はこちら</h3>
+                <h4>すでにアカウントを<br>お持ちの方はこちら</h4>
                 {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-primary btn-lg']) !!}
             </div>
         </div>
