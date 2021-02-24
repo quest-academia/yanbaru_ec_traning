aebc8024d479d9b852c8510e1811ffa8fd668f83
miyazaki0505 <mr.gingerman0505@gmail.com>
2021-02-24 13:03:32 +0000

    変数動作修正

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,12 +33,14 @@ class ProductController extends Controller
             $query->where('category_id', $categoryId)->get();
         }
 
-        //商品名・カテゴリーどちらか、もしくはどちらも、一致するものがなければ$messageを返す       
-        $message = "検索結果がありませんでした...";
-
         //$queryをcategory_idの昇順に並べて、$productsに代入
         //ページネーションは5商品ごとに指定
         $products = $query->orderBy('category_id', 'asc')->paginate(5);
+
+        //商品名・カテゴリーどちらか、もしくはどちらも、一致するものがなければ$messageを返す       
+        if (count($products) == 0) {
+            $message = "検索結果がありませんでした...";
+        }
 
         //M_Categoryモデル内getLists()関数を使用し、m_categoriesテーブルから「category_name」「id」を取得
         $category = new M_Category;
