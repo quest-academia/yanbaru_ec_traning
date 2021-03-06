738967262b1cccf07048cb67ee834c6d6a5b92e0
tomo-rrow86 <yu.tomo.ap1@gmail.com>
2021-03-06 07:06:26 +0000

    [update] BladeからJSを分離

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,17 +34,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
         <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
-        <script>
-
-        $(function(){
-        //フッターを最下部に固定
-            var $footer = $('#footer');
-            if(window.innerHeight > $footer.offset().top + $footer.outerHeight() ) {
-                $footer.attr({'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;' });
-            }
-        })
-
-        </script>
+        <script src="{{ asset('/js/jquery.js') }}"></script>
     </body>
 
 </html>
