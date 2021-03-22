257cc97ad55a3b6bcee5d48125d84e1fb8a1c2b8
nakasan773 <dskdsk773@gmail.com>
2021-03-22 16:54:30 +0000

    top画面の追加調整&コンフリクト解消（なかむ）

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
     <main>
-        <div class="container mt-5 text-center">
-                <span class="display-4 font-weight-bold">やんばるエキスパート　ECサイト</span>
-            </div>
+        <div class="container mt-5 pt-5 text-center">
+            <span class="display-4 font-weight-bold ">やんばるエキスパート　ECサイト</span>
+        </div>
     
             <div class="row justify-content-center mt-5">
-                <div class="col-3 text-center">
+                <div class="col-4 text-center">
                     <h3>まだアカウントを<br>お持ちでない方はこちら</h3>
-                    <button type="button" class="btn btn-primary">新規登録</button>
-
+                    <button type="button" class="btn btn-primary mt-5" style="width:120px;height:50px">新規登録</button>
                 </div>
-                <div class="col-3 text-center">
+                
+                <div class="col-4 text-center">
                     <h3>すでにアカウントを<br>お持ちの方はこちら</h3>
-                    <button type="button" class="btn btn-primary">ログイン</button>
+                    <button type="button" class="btn btn-primary mt-5" style="width:120px;height:50px">ログイン</button>
                 </div>
             </div>
     </main>
