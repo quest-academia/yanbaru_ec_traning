6caebc3fc25aa1c4537776df0bfc90eb02f1eed9
nakasan773 <dskdsk773@gmail.com>
2021-03-23 03:00:34 +0000

    インデント追加修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+
     <main>
         <div class="container mt-5 pt-5 text-center">
-            <span class="display-4 font-weight-bold ">やんばるエキスパート　ECサイト</span>
+            <span class="display-4 font-weight-bold">やんばるエキスパート　ECサイト</span>
         </div>
-    
-            <div class="row justify-content-center mt-5">
-                <div class="col-4 text-center">
-                    <h3>まだアカウントを<br>お持ちでない方はこちら</h3>
-                    <button type="button" class="btn btn-primary mt-5" style="width:120px;height:50px">新規登録</button>
-                </div>
-                
-                <div class="col-4 text-center">
-                    <h3>すでにアカウントを<br>お持ちの方はこちら</h3>
-                    <button type="button" class="btn btn-primary mt-5" style="width:120px;height:50px">ログイン</button>
-                </div>
+        
+        <div class="row justify-content-center mt-5">
+            <div class="col-4 text-center">
+                <h3>まだアカウントを<br>お持ちでない方はこちら</h3>
+                <button type="button" class="btn btn-primary mt-5" style="width:120px;height:50px">新規登録</button>
             </div>
+            
+            <div class="col-4 text-center">
+                <h3>すでにアカウントを<br>お持ちの方はこちら</h3>
+                <button type="button" class="btn btn-primary mt-5" style="width:120px;height:50px">ログイン</button>
+            </div>
+        </div>
     </main>
+
 @endsection
