df664b6bf3827965067735d43419bb6acf7e45ee
KawataTsutomu <tomtom.net1@gmail.com>
2021-03-27 09:05:29 +0000

    ログイン画面作成

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <main>
+      <div class="container mt-5 pt-5 text-center">
+          <h2>ログイン画面</h2>
+      </div>
+      {!! Form::open(['route' => 'login.post']) !!}
+          <form>
+            <div class="form-group">
+              <div class="row justify-content-center">
+                <div class="cal-xs-4">
+                  {!! Form::label('email', 'メールアドレス') !!}
+                  {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="row justify-content-center">
+                <div class="cal-xs-4">
+                  {!! Form::label('password', 'パスワード') !!}
+                  {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+              </div>
+            </div>
+
+            <div class="text-center">
+                {!! Form::submit('ログイン', ['class' => 'btn btn btn-primary']) !!}
+            </div>
+
+            <div class="text-center mt-3">
+                {!! link_to_route('signup', 'まだ登録がお済みでない方はこちら') !!}
+            </div>
+          </form>
+      {!! Form::close() !!}
+  </main>
+@endsection
