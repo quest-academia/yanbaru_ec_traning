a0633b0904bde14632a382c1356e7878f1f08f04
ziko7110 <ziko7110@gmail.com>
2021-04-05 01:44:58 +0900

    Auth修正

--- database/migrations/2021_03_15_140002_create_m_users_table.php
+++ database/migrations/2021_03_15_140002_create_m_users_table.php
@@ -27,8 +27,7 @@ class CreateMUsersTable extends Migration
             $table->integer('phone_number');
             $table->integer('user_classification_id')->unsigned(); //ユーザ種別 ->unsigned()追加
             $table->foreign('user_classification_id')->references('id')->on('m_user_classifications');//m_user_classificationsのidを参照する
-
-            $table->string('company_name', 128);
+            $table->string('company_name', 128)->nullable();
             $table->char('delete_flag', 1)->default(0); //デリートフラグ修正
         });
     }
