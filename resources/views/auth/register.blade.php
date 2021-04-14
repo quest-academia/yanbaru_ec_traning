694ef5f9ba5c21f238a9e55ba25036b437b0ce2c
tokuho0719 <gctsd383@yahoo.co.jp>
2021-04-14 11:27:59 +0000

    バリデーション:日本語化

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -144,6 +144,8 @@
         <h4>お客様情報登録</h4>
       </div>
 
+      @include('commons.error_messages') 
+
       <div class="row mt-5 mb-5">
         <div class="col-sm-5 mx-auto">
 
@@ -155,22 +157,22 @@
               <div class="ml-3">
                 <p class="d-inline">姓</p>
                 <input type="text" name="last_name" class="form-control-sm d-inline col-sm-5">
-                @if ($errors->has('last_name'))
+                <!-- @if ($errors->has('last_name'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('last_name') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
                 <p class="d-inline ml-1">名</p>
                 <input type="text" name="first_name" class="form-control-sm d-inline col-sm-5">
-                @if ($errors->has('first_name'))
+                <!-- @if ($errors->has('first_name'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('first_name') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
             </div>
 
@@ -178,13 +180,13 @@
               <label for="formGroupExampleInput2" class="d-block mt-3 mb-0">郵便番号</label>
               <div class="user-info">
                 <input type="text" name="zipcode" class="form-control-sm col-sm-6">
-                @if ($errors->has('zipcode'))
+                <!-- @if ($errors->has('zipcode'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('zipcode') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
             </div>
 
@@ -193,46 +195,46 @@
               <div class="clearfix">
                 <p class="d-inline ml-2">都道府県</p>
                 <input type="text" name="prefecture" class="form-control-sm col-sm-8 ml-1 d-inline float-right">
-                @if ($errors->has('prefecture'))
+                <!-- @if ($errors->has('prefecture'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('prefecture') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
               <div class="mt-1 clearfix">
                 <p class="d-inline ml-2">市町村区</p>
                 <input type="text" name="municipality" class="form-control-sm col-sm-8 ml-1 d-inline float-right">
-                @if ($errors->has('municipality'))
+                <!-- @if ($errors->has('municipality'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('municipality') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
               <div class="mt-1 clearfix">
                 <p class="d-inline ml-2">番地</p>
                 <input type="text" name="address" class="form-control-sm col-sm-8 ml-5 d-inline float-right">
-                @if ($errors->has('address'))
+                <!-- @if ($errors->has('address'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('address') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
               <div class="mt-1 clearfix">
                 <p class="d-block ml-2 mb-1">マンション・部屋番号</p>
                 <input type="text" name="apartments" class="form-control-sm col-sm-8 ml-1 d-inline float-right">
-                @if ($errors->has('apartments'))
+                <!-- @if ($errors->has('apartments'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('apartments') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
             </div>
 
@@ -240,13 +242,13 @@
               <label for="formGroupExampleInput2" class="mt-3 mb-0">メールアドレス</label>
               <div class="user-info width-control">
                 <input type="text" name="email" class="content-width form-control-sm d-inline">
-                @if ($errors->has('email'))
+                <!-- @if ($errors->has('email'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('email') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
             </div>
 
@@ -254,13 +256,13 @@
               <label for="formGroupExampleInput2" class="mt-3 mb-0">電話番号</label>
               <div class="user-info width-control">
                 <input type="text" name="phone_number" class="content-width form-control-sm d-inline">
-                @if ($errors->has('phone_number'))
+                <!-- @if ($errors->has('phone_number'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('phone_number') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
             </div>
 
@@ -268,13 +270,13 @@
               <label for="formGroupExampleInput2" class="d-block mt-3 mb-0">パスワード</label>
               <div class="user-info">
                 <input type="password" name="password" class="form-control-sm col-sm-8">
-                @if ($errors->has('password'))
+                <!-- @if ($errors->has('password'))
                     <div class="row justify-content-center">
                         <div class="cal-xs-4">
                             <span style="color:red">{{ $errors->first('password') }}</span>
                         </div>
                     </div>
-                @endif
+                @endif -->
               </div>
             </div>
 
--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,4 +1,4 @@
-@if (count($errors) > 0)
+<!-- @if (count($errors) > 0)
     <ul class="alert alert-danger">
         @foreach ($errors->all() as $error)
             <li class="ml-4">{{ $error }}</li>
@@ -6,4 +6,14 @@
     </ul>
 
 @endif
+-->
 
+@if ($errors->any())
+<div class="card-text text-left alert alert-danger">
+    <ul class="mb-0">
+    @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+    @endforeach
+    </ul>
+</div>
+@endif
