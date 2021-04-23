8dfb7ff8f14230573db19d16601a55bba2c7b9fa
riki-ichikawa <rkickw@gmail.com>
2021-04-23 02:25:07 +0000

    correct:パラメータ、改行、不要な処理削除

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
@@ -12,37 +11,29 @@ class OrdersController extends Controller
 {
     public function show(Request $request)
     {
-        //3ヶ月前の時間取得
-        $set_term_month = 3;
-        $months_before = Carbon::now()->subMonth($set_term_month);
-        
         //3ヶ月前の履歴表示：term_flg＝＞false, 全件履歴表示：term_flg＝＞true
-        if ($request->input('term_flg') == "false") {
-            $term_flg = false;
-        } else {
-            $term_flg = true;
-        }
-
+        $term_flg = $request->input('term_flg') == 'false' ? false : true;
+        
         //注文履歴情報取得
-        $orders_details = Order::where('user_id', Auth::id())
+        $orders = Order::where('user_id', Auth::id())
                         ->with('orderDetails', 'users')
-                        ->when(!($term_flg), function ($query) use ($months_before) {
-                            return $query->where('order_date', '>', $months_before);
+                        ->when(!($term_flg), function ($query) {
+                            return $query->where('order_date', '>', (Carbon::now()->subMonth(3)));
                         })
                         ->orderBy('order_date', 'desc')
                         ->paginate(15);
-
+        
         //注文状態が発送済：shipment_status_flg ＝ true, 準備中：shipment_status_flg ＝ false,
         $shipment_status_flg = false;//falseの場合は発送済
-        foreach ($orders_details as $order_info) {
-            foreach ($order_info->orderDetails as $order_details) {
-                if ($order_details->shipment_status_id == "1") {//shipment_status_id = 1 は 注文状態：'準備中'
+        foreach ($orders as $order) {
+            foreach ($order->orderDetails as $order_detail) {
+                if ($order_detail->shipment_status_id == "1") {//shipment_status_id = 1 は 注文状態：'準備中'
                     $shipment_status_flg = true;//trueの場合は準備中
                     break;
                 }
             }
         }
 
-        return view('order/order_history', compact('orders_details', 'term_flg', 'shipment_status_flg'));
+        return view('order/order_history', compact('orders', 'term_flg', 'shipment_status_flg'));
     }
 }
--- resources/views/order/order_history.blade.php
+++ resources/views/order/order_history.blade.php
@@ -13,7 +13,7 @@
                         <a class="btn btn-secondary btn-sm" href="{{ route('order.history', ['term_flg' => 'true']) }}" role="button" name="term">全件を表示</a>
                     @endif
                     </div>
-                    @if ($orders_details->isEmpty())
+                    @if ($orders->isEmpty())
                         <h1 class="text-center py-5">注文履歴は存在しません</h1>
                     @else
                         <div class="mt-2">
@@ -28,25 +28,29 @@
                                     </tr>
                                 </thead>
                                 <tbody class="" style="overflow-y:auto;max-height:400px;display:block">
-                                @foreach ($orders_details as $order_info)
+                                @foreach ($orders as $order)
                                     <tr class="d-flex">
                                         <th scope="row" class="col-1 px-0 text-center">{{ $loop->iteration }}</th>
-                                        <td class="col-2 px-0 text-center" id="order_number_1">{{ $order_info->id }}</td>
+                                            <td class="col-2 px-0 text-center" id="order_number_1">
+                                            @foreach ($order->orderDetails as $order_detail)
+                                                {{ $order_detail->order_detail_number }}<br>
+                                            @endforeach
+                                            </td>
                                         <td class="col-4 px-2">
-                                            <div>〒<span id="postal_code_1"></span>{{ $order_info->users->zipcode }}</div>
+                                            <div>〒<span id="postal_code_1"></span>{{ $order->users->zipcode }}</div>
                                             <div id="address_1">
-                                                {{ $order_info->users->prefecture }}
-                                                {{ $order_info->users->municipality }}
-                                                {{ $order_info->users->address }}
+                                                {{ $order->users->prefecture }}
+                                                {{ $order->users->municipality }}
+                                                {{ $order->users->address }}
                                             </div>
                                             <div id="order_name_1">
-                                                {{ $order_info->users->last_name }}
-                                                {{ $order_info->users->first_name }}
+                                                {{ $order->users->last_name }}
+                                                {{ $order->users->first_name }}
                                                 <span class="px-1">様</span>
                                             </div>
                                         </td>
                                         <td class="col-3 px-0">
-                                            <div>注文日時：<span id="order_date_2">{{ $order_info->order_date->format('Y-m-d') }}</span></div>
+                                            <div>注文日時：<span id="order_date_2">{{ $order->order_date->format('Y-m-d') }}</span></div>
                                             @if ($shipment_status_flg)
                                                 <div>注文状態：<span id="order_status_2">準備中</span></div>
                                             @else
@@ -59,7 +63,7 @@
                                 </tbody>
                             </table>
                             <div class="d-flex justify-content-center">
-                                {{ $orders_details->links() }}
+                                {{ $orders->links() }}
                             </div>
                         </div>
                     @endif
